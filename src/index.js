d35d0ce65aa2a37b4c1b16b4aed075137a118ff4
etcq <resonans93@gmail.com>
2025-10-30 19:15:02 +0500

    feat: add stdin for input command

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-import { stdout } from 'process';
+import { stdin } from 'process';
 
 const fileManager = async () => {
+  
   const args =  process.argv.slice(2);
-  const user =  args[0].split('=')[1];
+  const user = args.length > 0  ? args[0].split('=')[1] : 'unknown user';
   console.log(`Welcome to the File Manager, ${user}!`);
+  stdin.
+  stdin.on('data', (chunk) => {
+    const command = chunk.toString();``
+    if (command === '.exit') {
+      console.log(`Thank you for using File Manager, ${user}, goodbye!`);
+      process.exit();
+    } else {
+      console.log(command);
+    }
+
+  });
+  process.on('SIGINT', () => {
+    console.log(`Thank you for using File Manager, ${user}, goodbye!`);
+  })
 }
 
 fileManager();
